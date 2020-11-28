a4cd74cea53e82431c7778bb32fe2986f89c215c
dziugasp <dziugaspeciulevicius@gmail.com>
2020-11-28 23:09:27 +0200

    removed about page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :is_paid?
+  before_action :is_paid?, only: [:show]
   def is_paid?
     # check if user has paid for da memes
     # if not paid then restrict access
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
+
+# class ApplicationRecord < ActiveRecord::Base
+#   self.abstract_class = true
+#   validates :name, uniqueness: true
+#   validates :bikeType, presence: true
+# end
